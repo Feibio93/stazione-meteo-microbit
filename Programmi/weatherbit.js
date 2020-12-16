0e3c95b869621ebbcb9d87fa4777363b141273a2
Feibio93 <32126741+Feibio93@users.noreply.github.com>
2020-12-16 13:49:51 +0100

    Update weatherbit.js

--- Programmi/weatherbit.js
+++ Programmi/weatherbit.js
@@ -7,103 +7,34 @@ enum RadioMessage {
     Pioggia = 58249
 }
 function Azzera_array () {
+    Salva_su_scheda_SD()
     Altitudine = []
-    Direzione_vento = []
-    Velocita_vento = []
     Pioggia_caduta = []
     Pressione = []
     Temperatura_aria = []
     Temperatura_terreno = []
     Umidita_aria = []
     Umidita_terreno = []
+    Salva_direzione_vento = []
+    Salva_velocità_vento = []
 }
 radio.onReceivedMessage(RadioMessage.Umidita, function () {
     Tempo = Math.round(control.millis() / 1000 - Timestamp)
     radio.sendNumber(Tempo)
     basic.pause(200)
     radio.sendValue("HT", Umidita_terreno[Umidita_terreno.length - 1])
-    basic.pause(100)
+    basic.pause(200)
     radio.sendValue("HA", Umidita_aria[Umidita_aria.length - 1])
 })
-function Salva_dati_giornalieri (Nr_valori: number, Tipo: string) {
-    if (Nr_valori > 1) {
-        Indice_ogni_3_ore = Math.round(Nr_valori / 8)
-        Indice6 = Nr_valori
-        for (let index = 0; index < 8; index++) {
-            Calcola_media_ogni_3_ore(Indice6, Indice6 - Indice_ogni_3_ore, Tipo)
-            Indice6 = Indice6 - (Indice_ogni_3_ore + 1)
-        }
-        if (Tipo == "DV") {
-            for (let index = 0; index < 8; index++) {
-                // N S E W NE NW SE SW
-                Conta_direzioni_vento = [0, 0, 0, 0, 0, 0, 0, 0]
-                for (let index = 0; index < Indice_ogni_3_ore; index++) {
-                    if (Direzione_vento[Indice6] == "N") {
-                        Indice22 = 0
-                    } else if (Direzione_vento[Indice6] == "S") {
-                        Indice22 = 1
-                    } else if (Direzione_vento[Indice6] == "E") {
-                        Indice22 = 2
-                    } else if (Direzione_vento[Indice6] == "W") {
-                        Indice22 = 3
-                    } else if (Direzione_vento[Indice6] == "NE") {
-                        Indice22 = 4
-                    } else if (Direzione_vento[Indice6] == "NW") {
-                        Indice22 = 5
-                    } else if (Direzione_vento[Indice6] == "SE") {
-                        Indice22 = 6
-                    } else if (Direzione_vento[Indice6] == "SW") {
-                        Indice22 = 7
-                    }
-                    Conta_direzioni_vento.insertAt(Indice22, Conta_direzioni_vento[Indice22] + 1)
-                    Indice6 += -1
-                }
-                Indice22 = Conta_direzioni_vento[0]
-                for (let Indice = 0; Indice <= 7; Indice++) {
-                    if (Conta_direzioni_vento[Indice] > Indice22) {
-                        Indice22 = Conta_direzioni_vento[Indice]
-                    }
-                }
-                Indice22 = Conta_direzioni_vento.indexOf(Indice22)
-                if (Indice22 == 0) {
-                    Dato_direzione_vento = "N"
-                } else if (Indice22 == 1) {
-                    Dato_direzione_vento = "S"
-                } else if (Indice22 == 2) {
-                    Dato_direzione_vento = "E"
-                } else if (Indice22 == 3) {
-                    Dato_direzione_vento = "W"
-                } else if (Indice22 == 4) {
-                    Dato_direzione_vento = "NE"
-                } else if (Indice22 == 5) {
-                    Dato_direzione_vento = "NW"
-                } else if (Indice22 == 6) {
-                    Dato_direzione_vento = "SE"
-                } else if (Indice22 == 7) {
-                    Dato_direzione_vento = "SW"
-                }
-                Salva_direzione_vento.push(Dato_direzione_vento)
-            }
-        }
-    }
-}
 input.onButtonPressed(Button.A, function () {
     led.enable(true)
-    basic.showString("TEMP TERR:")
-    for (let Indice2 = 0; Indice2 <= Salva_temperatura_terreno.length - 1; Indice2++) {
-        basic.showNumber(Salva_temperatura_terreno[Indice2])
-    }
-    basic.showString("HUM TERR:")
-    for (let Indice3 = 0; Indice3 <= Salva_umidita_terreno.length - 1; Indice3++) {
-        basic.showNumber(Salva_umidita_terreno[Indice3])
-    }
     basic.showString("VEL VENTO:")
-    for (let Indice4 = 0; Indice4 <= Salva_velocita_vento.length - 1; Indice4++) {
-        basic.showNumber(Salva_velocita_vento[Indice4])
+    for (let Indice = 0; Indice <= Velocita_vento.length - 1; Indice++) {
+        basic.showNumber(Velocita_vento[Indice])
     }
     basic.showString("DIR VENTO:")
-    for (let Indice5 = 0; Indice5 <= Salva_direzione_vento.length - 1; Indice5++) {
-        basic.showString("" + (Salva_direzione_vento[Indice5]))
+    for (let Indice = 0; Indice <= Direzione_vento.length - 1; Indice++) {
+        basic.showString("" + (Direzione_vento[Indice]))
     }
     basic.clearScreen()
     led.enable(false)
@@ -118,109 +49,128 @@ radio.onReceivedMessage(RadioMessage.Pioggia, function () {
         radio.sendValue("PI", Pioggia_caduta[Pioggia_caduta.length - 1])
     }
 })
+function Media_velocità_vento (Inizio: number, Fine: number) {
+    Indice = Inizio
+    for (let index = 0; index < Fine; index++) {
+        Vento = Vento + Velocita_vento[Indice]
+        Indice += -1
+    }
+    Vento = Vento / (Fine + 1)
+    Velocita_vento = []
+    Salva_velocità_vento.push(Vento)
+    Vento = 0
+}
+function Media_direzione_vento (Inizio: number, Fine: number) {
+    Indice_ogni_5_minuti = Inizio
+    Indice2 = Fine
+    for (let index = 0; index < Fine; index++) {
+        // N S E W NE NW SE SW
+        Conta_direzioni_vento = [0, 0, 0, 0, 0, 0, 0, 0]
+        for (let index = 0; index < Indice_ogni_5_minuti; index++) {
+            if (Direzione_vento[Indice2] == "N") {
+                Indice3 = 0
+            } else if (Direzione_vento[Indice2] == "S") {
+                Indice3 = 1
+            } else if (Direzione_vento[Indice2] == "E") {
+                Indice3 = 2
+            } else if (Direzione_vento[Indice2] == "W") {
+                Indice3 = 3
+            } else if (Direzione_vento[Indice2] == "NE") {
+                Indice3 = 4
+            } else if (Direzione_vento[Indice2] == "NW") {
+                Indice3 = 5
+            } else if (Direzione_vento[Indice2] == "SE") {
+                Indice3 = 6
+            } else if (Direzione_vento[Indice2] == "SW") {
+                Indice3 = 7
+            }
+            Conta_direzioni_vento.insertAt(Indice3, Conta_direzioni_vento[Indice3] + 1)
+            Indice2 += -1
+        }
+        Indice2 = Conta_direzioni_vento[0]
+        for (let Indice = 0; Indice <= 7; Indice++) {
+            if (Conta_direzioni_vento[Indice] > Indice2) {
+                Indice2 = Conta_direzioni_vento[Indice]
+            }
+        }
+        Indice3 = Conta_direzioni_vento.indexOf(Indice2)
+        if (Indice3 == 0) {
+            Dato_direzione_vento = "N"
+        } else if (Indice3 == 1) {
+            Dato_direzione_vento = "S"
+        } else if (Indice3 == 2) {
+            Dato_direzione_vento = "E"
+        } else if (Indice3 == 3) {
+            Dato_direzione_vento = "W"
+        } else if (Indice3 == 4) {
+            Dato_direzione_vento = "NE"
+        } else if (Indice3 == 5) {
+            Dato_direzione_vento = "NW"
+        } else if (Indice3 == 6) {
+            Dato_direzione_vento = "SE"
+        } else if (Indice3 == 7) {
+            Dato_direzione_vento = "SW"
+        }
+        Direzione_vento = []
+        Salva_direzione_vento.push(Dato_direzione_vento)
+    }
+}
 radio.onReceivedMessage(RadioMessage.Temperatura, function () {
     Tempo = Math.round(control.millis() / 1000 - Timestamp)
     radio.sendNumber(Tempo)
-    //basic.pause(200)
-    //radio.sendValue("TT", Temperatura_terreno[Temperatura_terreno.length - 1])
-    basic.pause(100)
+    // basic.pause(200)
+    // radio.sendValue("TT", Temperatura_terreno[Temperatura_terreno.length - 1])
+    basic.pause(200)
     radio.sendValue("TA", Temperatura_aria[Temperatura_aria.length - 1])
 })
+function Salva_su_scheda_SD () {
+	
+}
 radio.onReceivedMessage(RadioMessage.Thingspeak, function () {
     if (Contatore > 0) {
-        //radio.sendValue("TTTS", Temperatura_terreno[Temperatura_terreno.length - 1])
-        //basic.pause(500)
+        // radio.sendValue("TTTS", Temperatura_terreno[Temperatura_terreno.length - 1])
+        // basic.pause(500)
         radio.sendValue("TATS", Temperatura_aria[Temperatura_aria.length - 1])
-        basic.pause(500)
+        basic.pause(200)
         radio.sendValue("HTTS", Umidita_terreno[Umidita_terreno.length - 1])
-        basic.pause(500)
+        basic.pause(200)
         radio.sendValue("HATS", Umidita_aria[Umidita_aria.length - 1])
-        basic.pause(500)
+        basic.pause(200)
         radio.sendValue("ALTS", Altitudine[Altitudine.length - 1])
-        basic.pause(500)
+        basic.pause(200)
         radio.sendValue("PRTS", Pressione[Pressione.length - 1])
-        basic.pause(500)
+        basic.pause(200)
         radio.sendValue("VVTS", Velocita_vento[Velocita_vento.length - 1])
         if (Umidita_terreno[Umidita_terreno.length - 1] > 85) {
             radio.sendValue("PITS", Pioggia_caduta[Pioggia_caduta.length - 1])
         }
     }
 })
-function Calcola_media_ogni_3_ore (Inizio: number, Fine: number, Tipo: string) {
-    Dato_ogni_3_ore = 0
-    Indice22 = Inizio
-    for (let index = 0; index < Inizio - Fine; index++) {
-        if (Tipo == "TT") {
-            Dato_ogni_3_ore = Dato_ogni_3_ore + Temperatura_terreno[Indice22]
-        } else if (Tipo == "TA") {
-            Dato_ogni_3_ore = Dato_ogni_3_ore + Temperatura_aria[Indice22]
-        } else if (Tipo == "HT") {
-            Dato_ogni_3_ore = Dato_ogni_3_ore + Umidita_terreno[Indice22]
-        } else if (Tipo == "HA") {
-            Dato_ogni_3_ore = Dato_ogni_3_ore + Umidita_aria[Indice22]
-        } else if (Tipo == "PR") {
-            Dato_ogni_3_ore = Dato_ogni_3_ore + Pressione[Indice22]
-        } else if (Tipo == "AL") {
-            Dato_ogni_3_ore = Dato_ogni_3_ore + Altitudine[Indice22]
-        } else if (Tipo == "VV") {
-            Dato_ogni_3_ore = Dato_ogni_3_ore + Velocita_vento[Indice22]
-        } else if (Tipo == "PI") {
-            Dato_ogni_3_ore = Dato_ogni_3_ore + Pioggia_caduta[Indice22]
-        }
-        Indice22 += -1
-    }
-    if (Tipo == "TT") {
-        Salva_temperatura_terreno.push(Dato_ogni_3_ore / (Inizio - Fine))
-    } else if (Tipo == "TA") {
-        let Salva_temperatura_aria: number[] = []
-        Salva_temperatura_aria.push(Dato_ogni_3_ore / (Inizio - Fine))
-    } else if (Tipo == "HT") {
-        Salva_umidita_terreno.push(Dato_ogni_3_ore / (Inizio - Fine))
-    } else if (Tipo == "HA") {
-        Salva_umidita_aria.push(Dato_ogni_3_ore / (Inizio - Fine))
-    } else if (Tipo == "PR") {
-        Salva_pressione.push(Dato_ogni_3_ore / (Inizio - Fine))
-    } else if (Tipo == "AL") {
-        Salva_altitudine.push(Dato_ogni_3_ore / (Inizio - Fine))
-    } else if (Tipo == "VV") {
-        Salva_velocita_vento.push(Dato_ogni_3_ore / (Inizio - Fine))
-    } else if (Tipo == "PI") {
-        Salva_pioggia_caduta.push(Dato_ogni_3_ore / (Inizio - Fine))
-    }
-}
 radio.onReceivedMessage(RadioMessage.Vento, function () {
-    Tempo = Math.round(control.millis() / 1000 - Timestamp)
-    radio.sendNumber(Tempo)
+    radio.sendValue("VV", Salva_velocità_vento[Salva_velocità_vento.length - 1])
     basic.pause(200)
-    radio.sendValue("VV", Velocita_vento[Velocita_vento.length - 1])
-    basic.pause(100)
-    radio.sendString("" + (Direzione_vento[Direzione_vento.length - 1]))
+    radio.sendString("" + (Salva_direzione_vento[Salva_direzione_vento.length - 1]))
 })
 radio.onReceivedMessage(RadioMessage.Pressione, function () {
     Tempo = Math.round(control.millis() / 1000 - Timestamp)
     radio.sendNumber(Tempo)
     basic.pause(200)
     radio.sendValue("PR", Pressione[Pressione.length - 1])
-    basic.pause(100)
+    basic.pause(200)
     radio.sendValue("AL", Altitudine[Altitudine.length - 1])
 })
-let Dato_ogni_3_ore = 0
 let Dato_direzione_vento = ""
-let Indice22 = 0
+let Indice3 = 0
 let Conta_direzioni_vento: number[] = []
-let Indice6 = 0
-let Indice_ogni_3_ore = 0
+let Indice2 = 0
+let Indice_ogni_5_minuti = 0
+let Indice = 0
 let Timestamp = 0
 let Tempo = 0
 let Contatore = 0
+let Vento = 0
 let Salva_direzione_vento: string[] = []
-let Salva_pioggia_caduta: number[] = []
-let Salva_velocita_vento: number[] = []
-let Salva_altitudine: number[] = []
-let Salva_pressione: number[] = []
-let Salva_umidita_terreno: number[] = []
-let Salva_umidita_aria: number[] = []
-let Salva_temperatura_terreno: number[] = []
+let Salva_velocità_vento: number[] = []
 let Direzione_vento: string[] = []
 let Velocita_vento: number[] = []
 let Altitudine: number[] = []
@@ -242,16 +192,11 @@ Pressione = []
 Altitudine = []
 Velocita_vento = []
 Direzione_vento = []
-Salva_temperatura_terreno = []
-Salva_temperatura_terreno = []
-Salva_umidita_aria = []
-Salva_umidita_terreno = []
-Salva_pressione = []
-Salva_altitudine = []
-Salva_velocita_vento = []
-Salva_pioggia_caduta = []
+Salva_velocità_vento = []
 Salva_direzione_vento = []
+Vento = 0
 Contatore = 0
+let Contatore_vento = 0
 basic.forever(function () {
     weatherbit.startWeatherMonitoring()
     weatherbit.startWindMonitoring()
@@ -267,6 +212,9 @@ basic.forever(function () {
     if (convertToText(Pressione[Pressione.length - 1]) == "NaN") {
         Pressione.pop()
     }
+    if (convertToText(Pressione[Pressione.length - 1]).length > 8) {
+        Pressione.insertAt(Pressione.length - 1, Math.round(Pressione[Pressione.length - 1] * 100) / 100)
+    }
     Altitudine.push(weatherbit.altitude())
     if (convertToText(Altitudine[Altitudine.length - 1]) == "NaN") {
         Altitudine.pop()
@@ -275,22 +223,18 @@ basic.forever(function () {
     if (Direzione_vento[Direzione_vento.length - 1] == "null") {
         Direzione_vento.pop()
     }
-    Velocita_vento.push(Math.round(weatherbit.windSpeed() * 1.60934 * 100) / 100)
-    if (convertToText(Velocita_vento[Velocita_vento.length - 1]) == "NaN") {
-        Velocita_vento.pop()
-    }
-    /*Temperatura_terreno.push(weatherbit.soilTemperature() / 100)
-    if (convertToText(Temperatura_terreno[Temperatura_terreno.length - 1]) == "NaN" || (Temperatura_terreno[Temperatura_terreno.length - 1] < -20 || Temperatura_terreno[Temperatura_terreno.length - 1] > 60)) {
-        while (Temperatura_terreno[Temperatura_terreno.length - 1] < -20 || Temperatura_terreno[Temperatura_terreno.length - 1] > 60) {
-            Temperatura_terreno.pop()
-            Temperatura_terreno.push(weatherbit.soilTemperature() / 100)
-        }
-    }*/
+    // Temperatura_terreno.push(weatherbit.soilTemperature() / 100)
+    // if (convertToText(Temperatura_terreno[Temperatura_terreno.length - 1]) == "NaN" || (Temperatura_terreno[Temperatura_terreno.length - 1] < -20 || Temperatura_terreno[Temperatura_terreno.length - 1] > 60)) {
+    // while (Temperatura_terreno[Temperatura_terreno.length - 1] < -20 || Temperatura_terreno[Temperatura_terreno.length - 1] > 60) {
+    // Temperatura_terreno.pop()
+    // Temperatura_terreno.push(weatherbit.soilTemperature() / 100)
+    // }
+    // }
     Umidita_terreno.push(Math.round(Math.map(weatherbit.soilMoisture(), 0, 1023, 0, 100)))
     if (convertToText(Umidita_terreno[Umidita_terreno.length - 1]) == "NaN") {
         Umidita_terreno.pop()
     }
-    if (Umidita_terreno[Umidita_terreno.length - 1] > 85) {
+    if (Umidita_aria[Umidita_aria.length - 1] > 85) {
         weatherbit.startRainMonitoring()
         Pioggia_caduta.push(Math.round(weatherbit.rain() * 25.4))
     }
@@ -299,15 +243,19 @@ basic.forever(function () {
     Contatore += 1
     if (Contatore == 17280) {
         Contatore = 0
-        //Salva_dati_giornalieri(Temperatura_terreno.length - 1, "TT")
-        Salva_dati_giornalieri(Temperatura_aria.length - 1, "TA")
-        Salva_dati_giornalieri(Umidita_terreno.length - 1, "HT")
-        Salva_dati_giornalieri(Umidita_aria.length - 1, "HA")
-        Salva_dati_giornalieri(Pressione.length - 1, "PR")
-        Salva_dati_giornalieri(Altitudine.length - 1, "AL")
-        Salva_dati_giornalieri(Velocita_vento.length - 1, "VV")
-        Salva_dati_giornalieri(Direzione_vento.length - 1, "DV")
-        Salva_dati_giornalieri(Pioggia_caduta.length - 1, "PI")
         Azzera_array()
     }
+})
+basic.forever(function () {
+    Velocita_vento.push(Math.round(weatherbit.windSpeed() * 1.60934 * 100) / 100)
+    if (convertToText(Velocita_vento[Velocita_vento.length - 1]) == "NaN") {
+        Velocita_vento.pop()
+    }
+    Contatore_vento += 1
+    if (Contatore_vento == 150) {
+        Media_velocità_vento(0, Velocita_vento.length - 1)
+        Media_direzione_vento(0, Direzione_vento.length - 1)
+        Contatore_vento = 0
+    }
+    basic.pause(2000)
 })
